ceefd59eeab9dece4e110a26faa385400eba7556
Curran <curran.kelleher@gmail.com>
2022-02-03 09:38:57 -0500

    Encode and decode page data

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import pkg from '../package.json';
 import { App } from './App';
+import { encode } from './pageData';
 
 const app = express();
 const port = 8080;
@@ -17,7 +18,7 @@ const backend = new ShareDB();
 const cdn = 'https://cdn.jsdelivr.net/npm';
 const reactVersion = pkg.dependencies.react.replace('^', '');
 
-export const indexHTML = (rootHTML) => `<!DOCTYPE html>
+export const indexHTML = (rootHTML, pageData) => `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -27,12 +28,13 @@ export const indexHTML = (rootHTML) => `<!DOCTYPE html>
     <div id="root">${rootHTML}</div>
     <script src="${cdn}/react@${reactVersion}/umd/react.production.min.js"></script>
     <script src="${cdn}/react-dom@${reactVersion}/umd/react-dom.production.min.js"></script>
+    <script>window.pageData = "${encode(pageData)}";</script>
     <script src="client.js"></script>
   </body>
 </html>`;
 
 app.get('/', (req, res) => {
-  res.send(indexHTML(renderToString(<App />)));
+  res.send(indexHTML(renderToString(<App />), { foo: 'bar' }));
 });
 
 app.use(express.static('build/public'));
